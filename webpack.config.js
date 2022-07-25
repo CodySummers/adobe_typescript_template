dbcd252e778ca924a3b7f84b966cec2c7ba85414
Gregory Wakefield <greg.wakefield@bytelondon.com>
2022-07-25 17:46:45 +0100

    watch mode added, when file is saved script re-runs

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 const path = require('path');
+const dirname = path.resolve()
 const packageName = process.env.npm_package_name;
+const exec = require('child_process').exec;
+const args = process.argv
+const application = args[args.length - 1]
+const adobeBuildScript = path.join(dirname, 'node_modules', 'adobe-build-scripts', 'bin', 'hostapps.js')
+const watchMode = args.some(arg => arg === '--watch')
 
 module.exports = {
     target: ['web', 'es3'],
@@ -22,5 +28,24 @@ module.exports = {
     },
     optimization: {
         minimize: false,
-      },
+    },
+    plugins: [
+        {
+            apply: (compiler) => {
+                compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+                    if (watchMode) {
+                        exec(`node ${adobeBuildScript} ${application}`, (err, stdout, stderr) => {
+                            if (stdout) process.stdout.write(stdout);
+                            if (stderr) process.stderr.write(stderr);
+                        });
+                    }
+                });
+            }
+        }
+    ]
 };
+
+
+
+
+
