b25fba88d728bbfcc30d7fc842d70777f7c17e98
Gregory Wakefield <greg.wakefield@bytelondon.com>
2022-08-28 14:15:37 +0100

    types

---
+++ src/lib/types.ts
@@ -0,0 +1,4 @@
+export type ExtendedEditText = EditText & { placeholder: string }
+
+export type AEArrayType = ItemCollection | _ItemClasses[]
+export type TypeSelection = typeof AVItem | typeof CompItem | typeof FolderItem | typeof FootageItem
