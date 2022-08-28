919dd792bf89e54186eda7e3fcc32e6313e9a433
Gregory Wakefield <greg.wakefield@bytelondon.com>
2022-08-28 14:14:35 +0100

    items by name

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,19 @@
+import { AEArrayType, TypeSelection } from "./types";
+
 export const testAlert = (text: string) => {
     alert(text)
+}
+
+export const itemsByType = (items: AEArrayType, itemType: TypeSelection) => {
+    const start = (items instanceof ItemCollection || items instanceof LayerCollection) ? 1 : 0
+    const end = start === 0 ? -1 : 0
+
+    const typeItems = []
+    for (let i = start; i <= items.length - end; i++) {
+        const item = items[i]
+        if (!(item instanceof itemType)) continue
+        typeItems.push(item)
+    }
+
+    return typeItems
 }
